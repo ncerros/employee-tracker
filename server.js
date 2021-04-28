edbb107b81a2b877509131e81d458ce4d9bfe36b
Norman Cerros <n_cerros@hotmail.com>
2021-04-28 12:58:14 -0600

    modified server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ connection.connect(function(err) {
         }
       });
   }
-  
+
 //Create a table to display a department which the user has chose
   function addDepartment() {
     inquirer
@@ -84,3 +84,39 @@ connection.connect(function(err) {
         });
       });
   }
+
+  function addRole() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "Choose the job title you want to add?",
+          name: "title"
+        },
+        {
+          type: "input",
+          message: "What is the salary for this position?",
+          name: "salary"
+        },
+        {
+          type: "input",
+          message: "Please choose the department ID for this position?",
+          name: "departmentID"
+        }
+      ])
+      .then(function(res) {
+        const title = res.title;
+        const salary = res.salary;
+        const departmentID = res.departmentID;
+        const query = `INSERT INTO role (title, salary, department_id) VALUE("${title}", "${salary}", "${departmentID}")`;
+        connection.query(query, function(err, res) {
+          if (err) throw err;
+          console.table(res);
+          start();
+        });
+      });
+  }
+
+
+
+  
