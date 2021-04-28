bec8232d4f58da333281c3fcb9df86c873d17e2e
Norman Cerros <n_cerros@hotmail.com>
2021-04-28 10:55:54 -0600

    modified server.js

--- server.js
+++ server.js
@@ -1,13 +1,67 @@
+// Dependencies required
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 
-// Establish server & PORT
-
+// Establishing server & PORT
 const connection = mysql.createConnection({
   host: "localhost",
-  port: 8889,
+  port: 8009,
   user: "root",
   password: "root",
   database: "tracker_DB"
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    start();
+  });
+// This functions will start the inquire prompt
+  function start() {
+    inquirer
+      .prompt({
+        type: "list",
+        name: "option",
+        message: "What do you like to do?",
+        choices: [
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "View Department",
+          "View Role",
+          "View Employee",
+          "Update Employee Role",
+          "Exit"
+        ]
+      })
+  
+      .then(function(result) {
+        console.log("You entered: " + result.option);
+        switch (result.option) {
+          case "Add Department":
+            addDepartment();
+            break;
+          case "Add Role":
+            addRole();
+            break;
+          case "Add Employee":
+            addEmployee();
+            break;
+          case "View Department":
+            viewDepartment();
+            break;
+          case "View Role":
+            viewRole();
+            break;
+          case "View Employee":
+            viewEmployee();
+            break;
+          case "Update Employee Role":
+            updateRole();
+            break;
+          case "Exit":
+            connection.end();
+            break;
+        }
+      });
+  }
